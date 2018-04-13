16d6e0331ed0493f4f7e0cb68630bebda8e8db0c
Antonin Houska <ah@cybertec.at>
2018-04-13 13:56:23 +0200

    Added comment on data alignment due to encryption.

--- src/include/storage/encryption.h
+++ src/include/storage/encryption.h
@@ -20,6 +20,21 @@
 #ifdef USE_OPENSSL
 /*
  * EVP_aes_256_xts() determines the following constants.
+ *
+ * Note: if the encryption algorithm changes and ENCRYPTION_BLOCK gets greater
+ * than MAXALIGN, make sure that the alignment of XLOG records is at least
+ * ENCRYPTION_BLOCK.
+ *
+ * If one XLOG record ended and the following one started in the same block,
+ * we'd have to either encrypt and decrypt both records together, or encrypt
+ * (after having zeroed the part of the block occupied by the other record)
+ * and decrypt them separate. Neither approach is compatible with streaming
+ * replication. In the first case we can't ask standby not to decrypt the
+ * first record until the second has been streamed. The second approach would
+ * imply streaming of two different versions of the same block two times.
+ *
+ * For similar reasons, the alignment to ENCRYPTION_BLOCK also has to be
+ * applied when storing changes to disk in reorderbuffer.c.
  */
 #define ENCRYPTION_BLOCK 1
 #define TWEAK_SIZE 16
