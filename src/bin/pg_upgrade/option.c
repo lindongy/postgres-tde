d3910f858291e78ae48c10fb6a1e610a2892802e
Antonin Houska <ah@cybertec.at>
2019-10-25 16:35:39 +0200

    Removed one more obsolete preprocessor directive.

--- src/bin/pg_upgrade/option.c
+++ src/bin/pg_upgrade/option.c
@@ -215,7 +215,6 @@ parseCommandLine(int argc, char *argv[])
 				break;
 
 #ifdef USE_ENCRYPTION
-#ifdef HAVE_UNIX_SOCKETS
 			case 'K':
 				encryption_key_command = pg_strdup(optarg);
 
@@ -223,15 +222,11 @@ parseCommandLine(int argc, char *argv[])
 				 * If the command is a command line option, it probably means
 				 * that the clusters do not have it in postgresql.conf, and
 				 * therefore we must pass it to pg_ctl when starting them.
-				 *
-				 * Unix socket is necessary for the key to be delivered to the
-				 * clusters safely, see pg_ctl.c.
 				 */
 				snprintf(encryption_key_command_opt,
 						 strlen(encryption_key_command) + 7,
 						 " -K '%s'", encryption_key_command);
 				break;
-#endif	/* HAVE_UNIX_SOCKETS */
 #endif	/* USE_ENCRYPTION */
 
 			default:
