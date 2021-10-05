b22487f46d5f1374eaaccaaa1039e1874c3a7db4
Antonin Houska <ah@cybertec.at>
2021-10-05 08:29:55 +0200

    Re-encrypt WAL when fetching historical timeline.
    Since the WAL can actually be fetched from a newer timeline than the receiver
    would expect and since the timeline is part of the encryption IV, the WAL must
    be decrypted and then encrypted using the requested timeline. Otherwise the
    receiver wouldn't be able to decrypt the data.

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -2563,6 +2563,7 @@ XLogSendPhysical(void)
 	Size		nbytes;
 	XLogSegNo	segno;
 	WALReadError errinfo;
+	TimeLineID	tli_req;
 
 	/* If requested switch the WAL sender to the stopping state. */
 	if (got_STOPPING)
@@ -2777,8 +2778,8 @@ XLogSendPhysical(void)
 	 * calls.
 	 */
 	enlargeStringInfo(&output_message, nbytes);
-
 retry:
+	tli_req = sendTimeLine;
 	if (!WALRead(xlogreader,
 				 &output_message.data[output_message.len],
 				 startptr,
@@ -2789,6 +2790,83 @@ retry:
 				 &errinfo,
 				 decrypt_stream))
 		WALReadRaiseError(&errinfo);
+
+	/*
+	 * As its comment explains, WalSndSegmentOpen() can open segment in a TLI
+	 * newer than the requested one. Since the TLI is used in the encryption
+	 * IV, we've got to decrypt the data and encrypt it while taking the new
+	 * TLI into account.
+	 */
+	if (data_encrypted && !decrypt_stream && tli_req != xlogreader->seg.ws_tli)
+	{
+		XLogRecPtr	reencr_ptr = startptr;
+		int	reencr_nbytes = nbytes;
+		int	skip = 0;
+		char	*data_ptr;
+		WALOpenSegment *seg = &xlogreader->seg;
+
+		/*
+		 * The WALRead() call above shouldn't have crossed more than one
+		 * segment boundary.
+		 */
+		Assert(nbytes < wal_segment_size);
+
+		/*
+		 * If the segment boundary is crossed, only the data from the second
+		 * segment need the re-encryption.
+		 */
+		if (reencr_ptr / wal_segment_size !=
+			(reencr_ptr + nbytes) / wal_segment_size)
+		{
+			skip = wal_segment_size - XLogSegmentOffset(reencr_ptr,
+														wal_segment_size);
+			reencr_ptr += skip;
+			reencr_nbytes -= skip;
+		}
+
+		/*
+		 * WALRead() should have read something from the new segment after
+		 * having it opened.
+		 */
+		Assert(reencr_nbytes > 0);
+
+		data_ptr = &output_message.data[output_message.len] + skip;
+
+		/* Process one page at a time. */
+		while (reencr_nbytes > 0)
+		{
+			PGAlignedXLogBlock buffer;
+			char		tweak[TWEAK_SIZE];
+			int	page_off = reencr_ptr % XLOG_BLCKSZ;
+			int	seg_off = (reencr_ptr - page_off) % wal_segment_size;
+			int	this_page = Min(XLOG_BLCKSZ - page_off, reencr_nbytes);
+
+			memcpy(buffer.data + page_off, data_ptr, this_page);
+			/* For decryption, use the TLI of the file actually read. */
+			XLogEncryptionTweak(tweak, seg->ws_tli, seg->ws_segno, seg_off);
+			decrypt_block(buffer.data,
+						  buffer.data,
+						  XLOG_BLCKSZ,
+						  tweak,
+						  InvalidBlockNumber,
+						  EDK_REL_WAL);
+
+			/* For encryption, use the TLI the receiver expects. */
+			XLogEncryptionTweak(tweak, tli_req, seg->ws_segno, seg_off);
+			encrypt_block(buffer.data,
+						  buffer.data,
+						  XLOG_BLCKSZ,
+						  tweak,
+						  InvalidXLogRecPtr,
+						  InvalidBlockNumber,
+						  EDK_REL_WAL);
+			memcpy(data_ptr, buffer.data + page_off, this_page);
+
+			reencr_ptr += this_page;
+			data_ptr += this_page;
+			reencr_nbytes -= this_page;
+		}
+	}
 
 	/* See logical_read_xlog_page(). */
 	XLByteToSeg(startptr, segno, xlogreader->segcxt.ws_segsize);
