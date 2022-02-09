lindongy/postgres-tde

commit 4d757bef2633746383fcb245f3171837837b1e91

Added an option to control the encryption key length.
So far, the key length was hard-wired to 128 bits. Now, the user can use the
--key-bits option to tell the initdb application which key length he
prefers. The accepted values are 128 (the default), 192 and 256 bits.

The key length information is stored in the control file, but also in the KDF
parameters file (global/kdf_params) because the pg_keytool utility may be
needed by initdb when the control file does not exist yet. This format change
implies that the KDF file needs to be migrated by pg_upgrade.

src/bin/pg_ctl/pg_ctl.c
@@ -114,7 +114,7 @@ static volatile pgpid_t postmasterPID = -1;
  * Define encryption_key locally rather that linking to storage/encryption.c
  * only because of this one variable.
  */
-extern unsigned char encryption_key[ENCRYPTION_KEY_LENGTH];
+extern unsigned char encryption_key[ENCRYPTION_KEY_MAX_LENGTH];
 #ifndef HAVE_UNIX_SOCKETS
 /*
  * Port at which postmaster listens for encryption key message.
@@ -186,6 +186,7 @@ static void unlimit_core_size(void);
 #endif
 
 static DBState get_control_dbstate(void);
+static int get_control_key_length(void);
 
 
 #ifdef WIN32
@@ -859,9 +860,9 @@ do_init(void)
 	{
 		size_t		len;
 
-		len = strlen(encryption_key_command) + 5;
+		len = strlen(encryption_key_command) + 7;
 		encr_opt_str = (char *) pg_malloc(len);
-		snprintf(encr_opt_str, len, " -K %s",
+		snprintf(encr_opt_str, len, " -K \"%s\"",
 				 encryption_key_command);
 	}
 	else
@@ -942,11 +943,13 @@ do_start(void)
 	if (encryption_key_command)
 #ifdef USE_ENCRYPTION
 	{
+		int		key_len = get_control_key_length();
+
 		/*
 		 * If encryption key is needed, retrieve it before trying to start
 		 * postmaster.
 		 */
-		run_encryption_key_command(pg_data);
+		run_encryption_key_command(pg_data, &key_len);
 	}
 #else
 	{
@@ -2473,6 +2476,23 @@ get_control_dbstate(void)
 	return ret;
 }
 
+static int
+get_control_key_length(void)
+{
+	int			ret;
+	bool		crc_ok;
+	ControlFileData *control_file_data = get_controlfile(pg_data, &crc_ok);
+
+	if (!crc_ok)
+	{
+		write_stderr(_("%s: control file appears to be corrupt\n"), progname);
+		exit(1);
+	}
+
+	ret = DATA_CIPHER_GET_KEY_LENGTH(control_file_data->data_cipher);
+	pfree(control_file_data);
+	return ret;
+}
 
 int
 main(int argc, char **argv)

src/bin/pg_upgrade/pg_upgrade.c
@@ -57,7 +57,7 @@ static char *get_encryption_key_command(char *bindir, char *pgconfig);
 static void prepare_new_cluster(void);
 static void prepare_new_globals(void);
 static void create_new_objects(void);
-static void copy_xact_xlog_xid(void);
+static void copy_xact_xlog_xid(EncryptionKey *encryption_key);
 static void set_frozenxids(bool minmxid_only);
 static void setup(char *argv0, bool *live_check);
 static void cleanup(void);
@@ -84,7 +84,7 @@ char encryption_key_command_opt[MAXPGPATH];
  * here.
  */
 bool		encryption_setup_done = false;
-extern unsigned char encryption_key[ENCRYPTION_KEY_LENGTH];
+extern unsigned char encryption_key[ENCRYPTION_KEY_MAX_LENGTH];
 
 int
 main(int argc, char **argv)
@@ -93,6 +93,7 @@ main(int argc, char **argv)
 	bool		live_check = false;
 #ifdef USE_ENCRYPTION
 	char	*key_cmd_pgconf;
+	EncryptionKey	encr_key_arg;
 #endif
 
 	pg_logging_init(argv[0]);
@@ -110,6 +111,24 @@ main(int argc, char **argv)
 	adjust_data_dir(&new_cluster);
 
 	setup(argv[0], &live_check);
+
+	output_check_banner(live_check);
+
+	check_cluster_versions();
+
+	get_sock_dir(&old_cluster, live_check);
+	get_sock_dir(&new_cluster, false);
+
+	check_cluster_compatibility(live_check);
+
+	/* Set mask based on PGDATA permissions */
+	if (!GetDataDirectoryCreatePerm(new_cluster.pgdata))
+		pg_fatal("could not read permissions of directory \"%s\": %s\n",
+				 new_cluster.pgdata, strerror(errno));
+
+	umask(pg_mode_mask);
+
+	check_and_dump_old_cluster(live_check);
 
 #ifdef USE_ENCRYPTION
 	/* Try to get the encryption key command from postgresql.conf. */
@@ -147,11 +166,21 @@ main(int argc, char **argv)
 	 */
 	if (encryption_key_command)
 	{
+		int	key_len;
+
 		/*
-		 * Both clusters should have the same KDF parameters, so we can pass
-		 * pgdata of any one.
+		 * We've already checked that both clusters use encryption key of the
+		 * same length.
 		 */
-		run_encryption_key_command(old_cluster.pgdata);
+		key_len = DATA_CIPHER_GET_KEY_LENGTH(old_cluster.controldata.data_cipher);
+
+		/*
+		 * Both clusters should eventually have KDF parameters of the old one,
+		 * so pass the old cluster's pgdata here.
+		 */
+		run_encryption_key_command(old_cluster.pgdata, &key_len);
+		encr_key_arg.data = encryption_key;
+		encr_key_arg.length = key_len;
 		encryption_setup_done = true;
 	}
 
@@ -165,25 +194,6 @@ main(int argc, char **argv)
 		Assert(false);
 	}
 #endif	/* USE_ENCRYPTION */
-
-	output_check_banner(live_check);
-
-	check_cluster_versions();
-
-	get_sock_dir(&old_cluster, live_check);
-	get_sock_dir(&new_cluster, false);
-
-	check_cluster_compatibility(live_check);
-
-	/* Set mask based on PGDATA permissions */
-	if (!GetDataDirectoryCreatePerm(new_cluster.pgdata))
-		pg_fatal("could not read permissions of directory \"%s\": %s\n",
-				 new_cluster.pgdata, strerror(errno));
-
-	umask(pg_mode_mask);
-
-	check_and_dump_old_cluster(live_check);
-
 
 	/* -- NEW -- */
 	start_postmaster(&new_cluster, true);
@@ -204,7 +214,7 @@ main(int argc, char **argv)
 	 * Destructive Changes to New Cluster
 	 */
 
-	copy_xact_xlog_xid();
+	copy_xact_xlog_xid(encryption_setup_done ? &encr_key_arg : NULL);
 
 	if (encryption_setup_done)
 #ifdef USE_ENCRYPTION
@@ -254,7 +264,7 @@ main(int argc, char **argv)
 	 */
 	prep_status("Setting next OID for new cluster");
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_setup_done ? &encr_key_arg : NULL,
 			  "\"%s/pg_resetwal\" -o %u \"%s\"",
 			  new_cluster.bindir,
 			  old_cluster.controldata.chkpnt_nxtoid,
@@ -587,7 +597,7 @@ copy_subdir_files(const char *old_subdir, const char *new_subdir)
 }
 
 static void
-copy_xact_xlog_xid(void)
+copy_xact_xlog_xid(EncryptionKey *encryption_key)
 {
 	/*
 	 * Copy old commit logs to new data dir. pg_clog has been renamed to
@@ -600,7 +610,7 @@ copy_xact_xlog_xid(void)
 
 	prep_status("Setting oldest XID for new cluster");
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_key,
 			  "\"%s/pg_resetwal\" -f -u %u \"%s\"",
 			  new_cluster.bindir, old_cluster.controldata.chkpnt_oldstxid,
 			  new_cluster.pgdata);
@@ -609,20 +619,20 @@ copy_xact_xlog_xid(void)
 	/* set the next transaction id and epoch of the new cluster */
 	prep_status("Setting next transaction ID and epoch for new cluster");
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_key,
 			  "\"%s/pg_resetwal\" -f -x %u \"%s\"",
 			  new_cluster.bindir,
 			  old_cluster.controldata.chkpnt_nxtxid,
 			  new_cluster.pgdata);
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_key,
 			  "\"%s/pg_resetwal\" -f -e %u \"%s\"",
 			  new_cluster.bindir,
 			  old_cluster.controldata.chkpnt_nxtepoch,
 			  new_cluster.pgdata);
 	/* must reset commit timestamp limits also */
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_key,
 			  "\"%s/pg_resetwal\" -f -c %u,%u \"%s\"",
 			  new_cluster.bindir,
 			  old_cluster.controldata.chkpnt_nxtxid,
@@ -649,7 +659,7 @@ copy_xact_xlog_xid(void)
 		 * counters here and the oldest multi present on system.
 		 */
 		exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-				  encryption_setup_done ? encryption_key : NULL,
+				  encryption_key,
 				  "\"%s/pg_resetwal\" -O %u -m %u,%u \"%s\"",
 				  new_cluster.bindir,
 				  old_cluster.controldata.chkpnt_nxtmxoff,
@@ -678,7 +688,7 @@ copy_xact_xlog_xid(void)
 		 * next=MaxMultiXactId, but multixact.c can cope with that just fine.
 		 */
 		exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-				  encryption_setup_done ? encryption_key : NULL,
+				  encryption_key,
 				  "\"%s/pg_resetwal\" -m %u,%u \"%s\"",
 				  new_cluster.bindir,
 				  old_cluster.controldata.chkpnt_nxtmulti + 1,
@@ -690,7 +700,7 @@ copy_xact_xlog_xid(void)
 	/* now reset the wal archives in the new cluster */
 	prep_status("Resetting WAL archives");
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
-			  encryption_setup_done ? encryption_key : NULL,
+			  encryption_key,
 	/* use timeline 1 to match controldata and no WAL history file */
 			  "\"%s/pg_resetwal\" -l 00000001%s \"%s\"", new_cluster.bindir,
 			  old_cluster.controldata.nextxlogfile + 8,

doc/src/sgml/encryption.sgml
@@ -33,12 +33,15 @@
 
  <para>
    The encryption key is expected in hexadecimal format, two characters
-   (hexadecimal digits) per byte. Since <productname>PostgreSQL</productname>
-   currently uses a key of length 16 bytes (128 bits), the expected length of
-   the key string is 32 characters. For example:
+   (hexadecimal digits) per byte. For the default key length of 128 bits (16
+   bytes), the expected length of the key string is 32 characters. For
+   example:
    <computeroutput>
      882fb7c12e80280fd664c69d2d636913
    </computeroutput>
+   Likewise, for the key lengths <literal>192</literal> and
+<literal>256</literal> bits, the key string length should
+   be <literal>48</literal> and <literal>64</literal> characters respectively.
  </para>
 
  <para>

doc/src/sgml/ref/initdb.sgml
@@ -218,6 +218,20 @@ PostgreSQL documentation
       </listitem>
      </varlistentry>
 
+     <varlistentry id="app-initdb-data-encr-key-length" xreflabel="data
+        encryption key length">
+      <term><option>--key-bits=<replaceable class="parameter">nbits</replaceable></option></term>
+      <listitem>
+       <para>
+         If the cluster should be encrypted, this option can be used to
+         specify the key length in bits. Accepted values
+         are <literal>128</literal> (the default), <literal>192</literal>
+         and <literal>256</literal>. See <xref linkend="encryption"/> for
+         information on the key format.
+       </para>
+      </listitem>
+     </varlistentry>
+
      <varlistentry id="app-initdb-data-checksums" xreflabel="data checksums">
       <term><option>-k</option></term>
       <term><option>--data-checksums</option></term>

doc/src/sgml/ref/pg_keytool.sgml
@@ -81,9 +81,10 @@ PostgreSQL documentation
       <listitem>
        <para>
         Specifies the directory where the database cluster is stored. In
-        particular, <application>pg_keytool</application> looks for
-        the <filename>global/kdf_params</filename> file here, see
-        <xref linkend="encryption_kdf_file"/>.
+        particular, <application>pg_keytool</application> reads
+        the <filename>global/kdf_params</filename> file from here (see
+        <xref linkend="encryption_kdf_file"/>), as well
+        as <filename>global/pg_control</filename>.
        </para>
 
        <para>

src/backend/access/transam/xlog.c
@@ -5075,8 +5075,12 @@ ReadControlFile(void)
 	 * Initialize encryption, but not if the current backend has already done
 	 * that.
 	 */
-	if (ControlFile->data_cipher > PG_CIPHER_NONE && !data_encrypted)
+	if (DATA_CIPHER_GET_KIND(ControlFile->data_cipher) != PG_CIPHER_NONE &&
+		!data_encrypted)
 	{
+		/* Save the data cipher. */
+		data_cipher = ControlFile->data_cipher;
+
 		/*
 		 * Set data_encryption for caller to know that he needs to retrieve
 		 * the key and initialize the encryption context.
@@ -5567,7 +5571,9 @@ BootStrapXLOG(void)
 	{
 		char	sample[ENCRYPTION_SAMPLE_SIZE];
 
-		ControlFile->data_cipher = PG_CIPHER_AES_CTR_128;
+		/* The bootstrap process should have initialized data_cipher. */
+		Assert(DATA_CIPHER_GET_KIND(data_cipher) != PG_CIPHER_NONE);
+		ControlFile->data_cipher = data_cipher;
 
 		sample_encryption(sample);
 
@@ -5576,7 +5582,8 @@ BootStrapXLOG(void)
 	}
 	else
 	{
-		ControlFile->data_cipher = PG_CIPHER_NONE;
+		DATA_CIPHER_CLEAR(data_cipher);
+		ControlFile->data_cipher = data_cipher;
 		memset(ControlFile->encryption_verification, 0,
 			   ENCRYPTION_SAMPLE_SIZE);
 	}

src/backend/bootstrap/bootstrap.c
@@ -389,19 +389,28 @@ AuxiliaryProcessMain(int argc, char *argv[])
 
 	/*
 	 * If data_encryption is set because of command line option, do the setup
-	 * now. (If set by postmaster, postmaster should have performed the
-	 * setup.)
+	 * now.
 	 *
 	 * This should only be useful for the bootstrap process. Anyone else
 	 * detects the encryption via ReadControlFile().
 	 */
 	if (data_encrypted && MyAuxProcType == BootstrapProcess)
 	{
+		int		key_length = 0;
+
 		Assert(!IsUnderPostmaster);
 		Assert(encryption_key_command &&
 			   strlen(encryption_key_command) > 0);
 
-		run_encryption_key_command(DataDir);
+		/*
+		 * User of the auxiliary process (typically initdb) should have
+		 * checked that the command returns a key of valid length. Thus we
+		 * don't need an extra command line option for it.
+		 */
+		run_encryption_key_command(DataDir, &key_length);
+		Assert(key_length == 16 || key_length == 24 || key_length == 32);
+
+		DATA_CIPHER_SET(data_cipher, PG_CIPHER_AES_CTR_CBC, key_length);
 
 		setup_encryption();
 	}

src/backend/postmaster/postmaster.c
@@ -542,7 +542,8 @@ typedef struct
 	int			MaxBackends;
 #ifdef USE_ENCRYPTION
 	bool		data_encrypted;
-	unsigned char encryption_key[ENCRYPTION_KEY_LENGTH];
+	uint8		data_cipher;
+	unsigned char encryption_key[ENCRYPTION_KEY_MAX_LENGTH];
 #endif
 #ifdef WIN32
 	HANDLE		PostmasterHandle;
@@ -1434,13 +1435,14 @@ PostmasterMain(int argc, char *argv[])
 	if (data_encrypted && !encryption_setup_done)
 	{
 		char	sample[ENCRYPTION_SAMPLE_SIZE];
+		int		key_length = DATA_CIPHER_GET_KEY_LENGTH(data_cipher);
 
 		/*
 		 * If the key command is in the configuration file, just run it,
 		 * otherwise let frontend application deliver it via FE/BE protocol.
 		 */
 		if (encryption_key_command)
-			run_encryption_key_command(DataDir);
+			run_encryption_key_command(DataDir, &key_length);
 		else
 		{
 			status = ServerLoop(true);
@@ -1456,7 +1458,8 @@ PostmasterMain(int argc, char *argv[])
 			 * Take a local copy of the key so that we don't have to receive
 			 * it again if restarting after a crash.
 			 */
-			memcpy(encryption_key, encryption_key_shmem, ENCRYPTION_KEY_LENGTH);
+			memcpy(encryption_key, encryption_key_shmem,
+				   ENCRYPTION_KEY_MAX_LENGTH);
 		}
 
 		/* Finalize the setup. */
@@ -2699,7 +2702,7 @@ processEncryptionKeyMessage(void *pkt)
 		return true;
 	}
 
-	memcpy(encryption_key, msg->data, ENCRYPTION_KEY_LENGTH);
+	memcpy(encryption_key, msg->data, ENCRYPTION_KEY_MAX_LENGTH);
 
 	return true;
 }
@@ -2759,7 +2762,8 @@ shareEncryptionKey(void)
 		}
 
 		ereport(LOG, (errmsg_internal("encryption key received")));
-		memcpy(encryption_key_shmem->data, encryption_key, ENCRYPTION_KEY_LENGTH);
+		memcpy(encryption_key_shmem->data, encryption_key,
+			   ENCRYPTION_KEY_MAX_LENGTH);
 	}
 
 	/*
@@ -6617,7 +6621,8 @@ save_backend_variables(BackendParameters *param, Port *port,
 	param->MaxBackends = MaxBackends;
 #ifdef USE_ENCRYPTION
 	param->data_encrypted = data_encrypted;
-	memcpy(param->encryption_key, encryption_key, ENCRYPTION_KEY_LENGTH);
+	param->data_cipher = data_cipher;
+	memcpy(param->encryption_key, encryption_key, ENCRYPTION_KEY_MAX_LENGTH);
 #endif
 
 #ifdef WIN32
@@ -6856,7 +6861,8 @@ restore_backend_variables(BackendParameters *param, Port *port)
 
 #ifdef USE_ENCRYPTION
 	data_encrypted = param->data_encrypted;
-	memcpy(encryption_key, param->encryption_key, ENCRYPTION_KEY_LENGTH);
+	data_cipher = param->data_cipher;
+	memcpy(encryption_key, param->encryption_key, ENCRYPTION_KEY_MAX_LENGTH);
 #endif
 
 #ifdef WIN32

commit 58056626f0a9119b4ee18e1b3693ea85947b855d

Fixed build w/o the --with-openssl option.
Broken by commit 4d757bef26.

src/bin/pg_ctl/pg_ctl.c
@@ -186,8 +186,9 @@ static void unlimit_core_size(void);
 #endif
 
 static DBState get_control_dbstate(void);
+#ifdef USE_ENCRYPTION
 static int get_control_key_length(void);
-
+#endif	/* USE_ENCRYPTION */
 
 #ifdef WIN32
 static void
@@ -2476,6 +2477,7 @@ get_control_dbstate(void)
 	return ret;
 }
 
+#ifdef USE_ENCRYPTION
 static int
 get_control_key_length(void)
 {
@@ -2493,6 +2495,7 @@ get_control_key_length(void)
 	pfree(control_file_data);
 	return ret;
 }
+#endif	/* USE_ENCRYPTION */
 
 int
 main(int argc, char **argv)

src/bin/pg_upgrade/pg_upgrade.c
@@ -214,7 +214,11 @@ main(int argc, char **argv)
 	 * Destructive Changes to New Cluster
 	 */
 
+#ifdef USE_ENCRYPTION
 	copy_xact_xlog_xid(encryption_setup_done ? &encr_key_arg : NULL);
+#else
+	copy_xact_xlog_xid(NULL);
+#endif	/* USE_ENCRYPTION */
 
 	if (encryption_setup_done)
 #ifdef USE_ENCRYPTION
@@ -264,7 +268,11 @@ main(int argc, char **argv)
 	 */
 	prep_status("Setting next OID for new cluster");
 	exec_prog(UTILITY_LOG_FILE, NULL, true, true,
+#ifdef USE_ENCRYPTION
 			  encryption_setup_done ? &encr_key_arg : NULL,
+#else
+			  NULL,
+#endif	/* USE_ENCRYPTION */
 			  "\"%s/pg_resetwal\" -o %u \"%s\"",
 			  new_cluster.bindir,
 			  old_cluster.controldata.chkpnt_nxtoid,