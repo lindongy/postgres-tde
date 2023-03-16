lindongy/postgres-tde

commit a21f5d18aa681296cb5ac1a0c885bfc1887b884f

Improve checks of the result of canAcceptConnections().
If backend starts during the CAC_OK phase, it can be one that only receives
the encryption key from the client and exits. However, so far we allowed such
a (dead_end) backend to proceed to ProcessStartupPacket() and return with
success, causing an assertion failure (due to uninitialized MyPMChildSlot).

So far the problem was only observed if the EXEC_BACKEND constat was set at
build time. The reason may be that regular fork() is fast enough to avoid this
race condition.

src/backend/postmaster/postmaster.c
@@ -388,6 +388,14 @@ static DNSServiceRef bonjour_sdref = NULL;
 /* Is the only purpose of this backend to receive encryption key? */
 static bool key_only_backend = false;
 
+/*
+ * Is it possible that the backend being started is there to receive the
+ * encryption key?
+ */
+#define MAY_BE_KEY_ONLY_BACKEND(cac) \
+	(data_encrypted && encryption_key_command == NULL && \
+	 (cac) == CAC_STARTUP)
+
 /* Has the "key only backend" received an empty key message? */
 static bool got_empty_key_msg = false;
 #endif	/* USE_ENCRYPTION */
@@ -2279,7 +2287,7 @@ ProcessStartupPacket(Port *port, bool ssl_done, bool gss_done)
 	 */
 	if (proto == ENCRYPTION_KEY_MSG_CODE)
 	{
-		if (data_encrypted)
+		if (MAY_BE_KEY_ONLY_BACKEND(port->canAcceptConnections))
 		{
 			if (ssl_done || gss_done)
 				ereport(DEBUG1,
@@ -4584,7 +4592,7 @@ BackendStartup(Port *port)
 	 */
 	cac = port->canAcceptConnections = canAcceptConnections(BACKEND_TYPE_NORMAL);
 	bn->dead_end = (port->canAcceptConnections != CAC_OK &&
-					!(data_encrypted && cac == CAC_STARTUP));
+					!MAY_BE_KEY_ONLY_BACKEND(cac));
 
 	/*
 	 * Unless it's a dead_end child, assign it a child slot number

commit 07f2c4db880e263f0dabb961ccb6dc272773dd6e

Fix build without the --with-openssl option.
The macro MAY_BE_KEY_ONLY_BACKEND should be defined regardless the
--with-openssl configure option. Bug introduced by commit a21f5d18aa.

src/backend/postmaster/postmaster.c
@@ -388,6 +388,10 @@ static DNSServiceRef bonjour_sdref = NULL;
 /* Is the only purpose of this backend to receive encryption key? */
 static bool key_only_backend = false;
 
+/* Has the "key only backend" received an empty key message? */
+static bool got_empty_key_msg = false;
+#endif	/* USE_ENCRYPTION */
+
 /*
  * Is it possible that the backend being started is there to receive the
  * encryption key?
@@ -395,10 +399,6 @@ static bool key_only_backend = false;
 #define MAY_BE_KEY_ONLY_BACKEND(cac) \
 	(data_encrypted && encryption_key_command == NULL && \
 	 (cac) == CAC_STARTUP)
-
-/* Has the "key only backend" received an empty key message? */
-static bool got_empty_key_msg = false;
-#endif	/* USE_ENCRYPTION */
 
 /*
  * postmaster.c - function prototypes